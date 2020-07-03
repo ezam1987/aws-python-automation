f01e4ae36bfaf308fd36c6a85b91fcac3df36a2d
Emmanuel Alvarez <61366379+ezam1987@users.noreply.github.com>
2020-07-02 22:10:08 -0500

    modifying setup.py and another information

--- 01-webotron/setup.py
+++ 01-webotron/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='webotron-80',
     version='0.1',
-    author='Robin Norwood',
-    author_email='robin@acloud.guru',
+    author='Emmamnuel Alvarez',
+    author_email='emmanuel1787@gmail.com',
     description='Webotron 80 is a tool to deploy static websites to AWS.',
     license='GPLv3+',
     packages=['webotron'],
-    url='https://github.com/ACloudGuru/automating-aws-with-python/tree/master/01-webotron',
+    url='https://github.com/ezam1987/aws-python-automation/edit/master/01-webotron',
     install_requires=[
         'click',
         'boto3'
